875591fc50503382e2c0e0826df3fb1d04e5bac9
karthickpandiyan <pandiyan289@gmail.com>
2019-12-25 08:43:10 +0530

    Completed cities recycler adapter to load list of data into it

--- app/src/main/java/com/kp/androidworldcities/ui/adapter/CitiesRecyclerAdapter.kt
+++ app/src/main/java/com/kp/androidworldcities/ui/adapter/CitiesRecyclerAdapter.kt
@@ -1,17 +1,38 @@
 package com.kp.androidworldcities.ui.adapter
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.kp.androidworldcities.R
 import com.kp.androidworldcities.model.Cities
 import kotlinx.android.synthetic.main.item_cities.view.*
 
 
-class CitiesRecyclerAdapter(private val context: Context, private var citiesList: List<Cities>) {
+class CitiesRecyclerAdapter(private val context: Context, private var citiesList: List<Cities>):
+    RecyclerView.Adapter<CitiesRecyclerAdapter.CityViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CityViewHolder {
+        return CityViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.item_cities, parent, false)
+        )
+    }
+
+    override fun getItemCount() = citiesList.size
+
+    override fun onBindViewHolder(holer: CityViewHolder, position: Int) {
+        holer.bind(citiesList[position])
+    }
 
     class CityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(city: Cities) {
             itemView.tvCityTitle.text = city.getCityAndCountryTitle()
         }
+    }
+
+    fun loadCities(citiesList: List<Cities>) {
+        this.citiesList = citiesList
+        notifyDataSetChanged()
     }
 }
