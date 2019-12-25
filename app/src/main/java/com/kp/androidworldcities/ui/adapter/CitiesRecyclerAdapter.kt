56be5efbd1b82aedb472af7720baf1831ca24fad
karthickpandiyan <pandiyan289@gmail.com>
2019-12-25 08:41:27 +0530

    Created view holder class to hold the item view

--- app/src/main/java/com/kp/androidworldcities/ui/adapter/CitiesRecyclerAdapter.kt
+++ app/src/main/java/com/kp/androidworldcities/ui/adapter/CitiesRecyclerAdapter.kt
@@ -1,5 +1,17 @@
 package com.kp.androidworldcities.ui.adapter
 
-class CitiesRecyclerAdapter {
+import android.content.Context
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.kp.androidworldcities.model.Cities
+import kotlinx.android.synthetic.main.item_cities.view.*
 
+
+class CitiesRecyclerAdapter(private val context: Context, private var citiesList: List<Cities>) {
+
+    class CityViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(city: Cities) {
+            itemView.tvCityTitle.text = city.getCityAndCountryTitle()
+        }
+    }
 }
