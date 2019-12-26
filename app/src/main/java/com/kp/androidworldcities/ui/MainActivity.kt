bb12b3727d7c210f0cd0ec86bd1c59087190ec8d
karthickpandiyan <pandiyan289@gmail.com>
2019-12-26 11:05:22 +0530

    Implement search logic to filter data from list of cities using RxJava.

--- app/src/main/java/com/kp/androidworldcities/ui/MainActivity.kt
+++ app/src/main/java/com/kp/androidworldcities/ui/MainActivity.kt
@@ -6,17 +6,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.jakewharton.rxbinding2.widget.textChanges
 import com.kp.androidworldcities.R
 import com.kp.androidworldcities.model.Cities
 import com.kp.androidworldcities.ui.adapter.CitiesRecyclerAdapter
 import com.kp.androidworldcities.viewmodel.CitiesViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.schedulers.Schedulers
 
 import kotlinx.android.synthetic.main.content_main.*
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var citiesViewModel: CitiesViewModel
-    lateinit var citiesAdapter : CitiesRecyclerAdapter;
+    lateinit var citiesAdapter : CitiesRecyclerAdapter
+    private val disposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,10 +37,25 @@ class MainActivity : AppCompatActivity() {
                 progressBar.visibility = View.GONE
                 citiesViewModel.configureCitiesData(it)
                 setCitiesListAdapter(it)
+                initializeSearch()
             }
         })
+    }
 
-
+    private fun initializeSearch() {
+        edtSearchInput
+            .textChanges()
+            .debounce(200, TimeUnit.MILLISECONDS)
+            .subscribe {
+                citiesViewModel.doSearch(it.toString())
+                    .subscribeOn(Schedulers.computation())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe {
+                        citiesViewModel.existingCitiesList.clear()
+                        citiesViewModel.existingCitiesList.addAll(citiesViewModel.filteredCitiesList)
+                        setCitiesListAdapter(citiesViewModel.existingCitiesList)
+                    }.addTo(disposable)
+            }.addTo(disposable)
     }
 
     private fun setupRecyclerAdapter() {
