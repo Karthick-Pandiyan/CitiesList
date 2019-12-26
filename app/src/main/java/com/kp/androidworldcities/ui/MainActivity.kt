26e7047a22795583f27b34bdfce274828fe667d4
karthickpandiyan <pandiyan289@gmail.com>
2019-12-26 10:57:51 +0530

    Read list of cities from observer and load it to adapter

--- app/src/main/java/com/kp/androidworldcities/ui/MainActivity.kt
+++ app/src/main/java/com/kp/androidworldcities/ui/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.kp.androidworldcities.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kp.androidworldcities.R
+import com.kp.androidworldcities.model.Cities
 import com.kp.androidworldcities.ui.adapter.CitiesRecyclerAdapter
 import com.kp.androidworldcities.viewmodel.CitiesViewModel
 
@@ -24,7 +26,11 @@ class MainActivity : AppCompatActivity() {
         setupRecyclerAdapter()
 
         citiesViewModel.getCitiesList.observe(this, Observer { citiesList ->
-
+            citiesList?.let {
+                progressBar.visibility = View.GONE
+                citiesViewModel.configureCitiesData(it)
+                setCitiesListAdapter(it)
+            }
         })
 
 
@@ -34,5 +40,9 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         citiesAdapter = CitiesRecyclerAdapter(this, listOf())
         recyclerView.adapter = citiesAdapter
+    }
+
+    private fun setCitiesListAdapter(citiesList: List<Cities>) {
+        citiesAdapter.loadCities(citiesList)
     }
 }
