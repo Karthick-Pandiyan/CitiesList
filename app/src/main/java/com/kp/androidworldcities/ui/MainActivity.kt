bcaa63cd5e91433833555978053ca044f82261f3
karthickpandiyan <pandiyan289@gmail.com>
2019-12-26 10:53:15 +0530

    Initialize adapter to load list of cites in recycler view

--- app/src/main/java/com/kp/androidworldcities/ui/MainActivity.kt
+++ app/src/main/java/com/kp/androidworldcities/ui/MainActivity.kt
@@ -4,26 +4,35 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.kp.androidworldcities.R
+import com.kp.androidworldcities.ui.adapter.CitiesRecyclerAdapter
 import com.kp.androidworldcities.viewmodel.CitiesViewModel
 
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var citiesViewModel: CitiesViewModel
+    lateinit var citiesAdapter : CitiesRecyclerAdapter;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         citiesViewModel = ViewModelProviders.of(this).get(CitiesViewModel::class.java)
+        setupRecyclerAdapter()
 
-        citiesViewModel.getCitiesList.observe(this, Observer {
+        citiesViewModel.getCitiesList.observe(this, Observer { citiesList ->
 
         })
 
 
+    }
+
+    private fun setupRecyclerAdapter() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        citiesAdapter = CitiesRecyclerAdapter(this, listOf())
+        recyclerView.adapter = citiesAdapter
     }
 }
